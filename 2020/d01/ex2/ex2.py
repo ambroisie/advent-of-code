6d30ed5992a19cf3fc57416348366fdf3cffeb78
Bruno BELANYI <bruno@belanyi.fr>
2020-12-01 16:36:56 +0100

    2020: d01: ex2: add solution

---
+++ 2020/d01/ex2/ex2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import functools
+import itertools
+import operator
+import sys
+
+
+def main() -> None:
+    values = [int(n) for n in sys.stdin.readlines()]
+    for tup in itertools.combinations(values, 3):
+        if sum(tup) == 2020:
+            print(functools.reduce(operator.mul, tup))
+            break
+
+
+if __name__ == "__main__":
+    main()

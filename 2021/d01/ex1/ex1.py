b1bec40137f6540ffe52430b63ce680db0a57fc8
Bruno BELANYI <bruno@belanyi.fr>
2021-12-01 10:43:29 +0100

    2021: d01: ex1: add solution

---
+++ 2021/d01/ex1/ex1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import itertools
+import sys
+from typing import List
+
+
+def solve(input: List[int]) -> int:
+    return sum(prev < cur for (prev, cur) in zip(input, input[1:]))
+
+
+def main() -> None:
+    input = [int(line) for line in sys.stdin.readlines()]
+    print(solve(input))
+
+
+if __name__ == "__main__":
+    main()

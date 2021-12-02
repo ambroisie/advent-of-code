7956b96baa24ef8eaa2d5d198e087743ce4e8a74
Bruno BELANYI <bruno@belanyi.fr>
2021-12-02 10:10:45 +0100

    2021: d02: ex2: add solution

---
+++ 2021/d02/ex2/ex2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import itertools
+import sys
+from typing import List
+
+
+def solve(input: List[str]) -> int:
+    x, y, aim = 0, 0, 0
+
+    for instruction in input:
+        dir, length_ = instruction.split(" ")
+        length = int(length_)
+        if dir == "forward":
+            x += length
+            y += length * aim
+        elif dir == "down":
+            aim += length
+        elif dir == "up":
+            aim -= length
+        else:
+            assert False
+
+    return x * y
+
+
+def main() -> None:
+    input = [line.strip() for line in sys.stdin.readlines()]
+    print(solve(input))
+
+
+if __name__ == "__main__":
+    main()

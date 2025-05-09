511e8abb0a5676502beaefbacebb5367c116d9a8
Bruno BELANYI <bruno@belanyi.fr>
2025-05-09 16:11:23 +0100

    2017: d04: ex1: add solution

---
+++ 2017/d04/ex1/ex1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def solve(input: str) -> int:
+    def parse(input: str) -> list[list[str]]:
+        return [line.split() for line in input.splitlines()]
+
+    def validate_passphrase(passphrase: list[str]) -> bool:
+        return len(set(passphrase)) == len(passphrase)
+
+    passphrases = parse(input)
+    return sum(map(validate_passphrase, passphrases))
+
+
+def main() -> None:
+    input = sys.stdin.read()
+    print(solve(input))
+
+
+if __name__ == "__main__":
+    main()

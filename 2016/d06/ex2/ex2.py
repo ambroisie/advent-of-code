c8b28ef2ef6df7a07dec2859bf5df82d66dfc572
Bruno BELANYI <bruno@belanyi.fr>
2025-05-19 01:18:32 +0100

    2016: d06: ex2: add solution

---
+++ 2016/d06/ex2/ex2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import collections
+import sys
+
+
+def solve(input: str) -> str:
+    def error_correct(messages: list[str]) -> str:
+        res: list[str] = []
+        for i in range(len(messages[0])):
+            res.append(collections.Counter(l[i] for l in messages).most_common()[-1][0])
+        return "".join(res)
+
+    messages = input.splitlines()
+    return error_correct(messages)
+
+
+def main() -> None:
+    input = sys.stdin.read()
+    print(solve(input))
+
+
+if __name__ == "__main__":
+    main()

b3642638385d3c9cbe579d980b630382b6aab13b
Bruno BELANYI <bruno@belanyi.fr>
2025-05-21 00:59:17 +0100

    2015: d04: ex1: add solution

---
+++ 2015/d04/ex1/ex1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import hashlib
+import itertools
+import sys
+
+
+def solve(input: str) -> int:
+    key = input.strip()
+    for i in itertools.count(1):
+        hash = hashlib.md5((key + str(i)).encode()).hexdigest()
+        if hash.startswith("00000"):
+            return i
+    assert False  # Sanity check
+
+
+def main() -> None:
+    input = sys.stdin.read()
+    print(solve(input))
+
+
+if __name__ == "__main__":
+    main()

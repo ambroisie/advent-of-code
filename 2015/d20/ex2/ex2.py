235f801f412f4fd60c0cb5504a3a57fd7d129c37
Bruno BELANYI <bruno@belanyi.fr>
2025-05-22 00:44:09 +0100

    2015: d20: ex2: add solution

---
+++ 2015/d20/ex2/ex2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def solve(input: str) -> int:
+    needed = int(input.strip())
+    presents = [0] * (needed // 10)  # Surely this should be enough houses
+    for i in range(1, len(presents)):
+        for j in range(1, 50 + 1):
+            if (i * j) >= len(presents):
+                continue
+            presents[i * j] += 11 * i
+    for i, total in enumerate(presents):
+        if total >= needed:
+            return i
+    assert False  # Sanity check
+
+
+def main() -> None:
+    input = sys.stdin.read()
+    print(solve(input))
+
+
+if __name__ == "__main__":
+    main()
